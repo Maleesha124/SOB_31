790a45a790822b1462ee2197338297683724db02
Maleesha Chamodi <maleeshachamodi05@gmail.com>
2023-11-28 11:44:55 +0400

    debugged and removed errors from cowbulls_incomplete.py

--- cowbulls_incomplete.py
+++ cowbulls_incomplete.py
@@ -2,12 +2,21 @@ import random
 
 def compare_numbers(number, user_guess):
     ## your code here
-    return cowbull
+    cows = 0 #counting cows
+    bulls = 0 #counting bulls
+
+    for i in range(len(number)): #for the random 4 digit number generated
+        if user_guess[i] == number[i]: #if the user guesses the number correctly, 1 bull is added
+            bulls+=1
+        elif user_guess[i] in number: #if the user guesses the number incorrectly, 1 cow is added
+            cows+=1
+            
+    return cows, bulls #changed the return to fit the code
 
 playing = True #gotta play the game
 number = str(random.randint(0,9999)) #random 4 digit number
 guesses = 0
-print number
+#removed this line as it removes the purpose of the game
 
 print("Let's play a game of Cowbull!") #explanation
 print("I will generate a number, and you have to guess the numbers one digit at a time.")
@@ -16,7 +25,7 @@ print("The game ends when you get 4 bulls!")
 print("Type exit at any prompt to exit.")
 
 while playing:
-    user_guess = raw_input("Give me your best guess!")
+    user_guess = input("Give me your best guess!")#corrected 'raw_input' to 'input'
     if user_guess == "exit":
         break
     cowbullcount = compare_numbers(number,user_guess)
@@ -26,7 +35,8 @@ while playing:
 
     if cowbullcount[1]==4:
         playing = False
-        print("You win the game after " + str(guesses) + "! The number was "+str(number))
+        print("You win the game after " + str(guesses) + "guesses! The number was "+str(number))#added the word guesses
         break #redundant exit
     else:
         print("Your guess isn't quite right, try again.")
+
